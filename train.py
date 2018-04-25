f0af4993f75e67ffe7dacaca22852e74bde09a07
yasminalaa <yasminalaa96@hotmail.com>
2018-04-25 15:41:46 +0200

    resumefrom epoch 95

--- train.py
+++ train.py
@@ -53,7 +53,7 @@ def main():
 	parser.add_argument('--save_every', type=int, default=30,
 					   help='Save Model/Samples every x iterations over batches')
 
-	parser.add_argument('--resume_model', type=str, default=None,
+	parser.add_argument('--resume_model', type=str, default='Data/Models/model_after_flowers_epoch_95.ckpt.data-00000-of-00001',
                        help='Pre-Trained Model Path, to resume from')
 
 	parser.add_argument('--data_set', type=str, default="flowers",
